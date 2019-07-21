be15ad3a2454b7111168d595bc4c28437b717912
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-21 06:15:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   
   def reverse_transfer
     if @status = "complete"
-      @sender.deposit( @amount )
+      @sender.balance( @amount )
       @reciever.deposit( @amount * -1 )
       @status = "reversed"
     end
